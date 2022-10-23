3dc3efb9f54ecd428d1ae1dfbe2f86e8892651e7
Juliet Linarez <julietlinarez@gmail.com>
2022-10-23 22:50:51 +0200

    fixes in keypad in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,29 +13,25 @@
                     <h5 class="txtcard">{{$image->title}}</h5>
                 </div>
                 <div class="keypad">
-                    <form action="{{ route('imagen.fav',['id'=>$image->id]) }}" method="POST">
+                    <form  action="{{ route('imagen.fav',['id'=>$image->id]) }}" method="POST">
                         <input type="hidden" name="fav" value="{{$image->favorite}}">
-                        <button type="submit">
+                        <button class="btn" type="submit">
                             @if ($image->favorite == 1)
                                 <a href="#" class="H-btn"><i class="bi bi-heart-fill"></i></a>
                             @else
                                 <a href="#" class="H-btn"><i class="bi bi-heart"></i></a>
                             @endif
-
                         </button>
                         @csrf
                         @method('PUT')
                     </form>
-
-                    
                     <form action="{{ route('imagen.destroy',['id'=>$image->id]) }}" method="POST">
-                        <button type="submit">
+                        <button class="btn" type="submit">
                             <a href="#" class="T-btn"><i class="bi bi-trash3"></i></a>
                         </button>
                         @csrf
                         @method('DELETE')
                     </form>
-                    
                     <a href="{{ route('imagen.edit',['id'=>$image->id]) }}" class="P-btn"><i class="bi bi-pencil"></i></a>
                 </div>
             </div>
